176a6ef49696c93c6ab4195f6c421c2257e46f49
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-01-10 17:45:20 -0500

    refactor(channels): remove channel routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,14 +2,12 @@ use axum::Router;
 
 use crate::AppState;
 
-pub mod channels;
 pub mod emotes;
 pub mod sets;
 pub mod users;
 
 pub fn router() -> Router<AppState> {
 	Router::new()
-		.merge(self::channels::router())
 		.merge(self::emotes::router())
 		.merge(self::sets::router())
 		.merge(self::users::router())
