df01f0c554a2bfbe68a0e3a7a170f4017d306423
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-01-15 18:33:29 -0500

    refactor: cleanup logging

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ mod snowflake;
 
 use std::time::Duration;
 
+use axum::http::Request;
 use axum::Router;
 use shuttle_secrets::SecretStore;
 use sqlx::postgres::PgPoolOptions;
 use tower::ServiceBuilder;
 use tower_http::timeout::TimeoutLayer;
 use tower_http::trace::TraceLayer;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::EnvFilter;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
 
 use crate::error::Error;
 
@@ -26,15 +26,15 @@ struct AppState {
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_secrets::Secrets] secrets: SecretStore) -> shuttle_axum::ShuttleAxum {
-	dotenvy::dotenv().expect("Loading .env failed");
+	dotenvy::dotenv().expect("Loading `.env` failed");
 
 	tracing_subscriber::registry()
-		.with(EnvFilter::try_from_default_env().unwrap_or_else(|_| "api=debug".into()))
-		.with(tracing_subscriber::fmt::layer())
+		.with(EnvFilter::new("api=info,tower_http=debug"))
+		.with(fmt::layer())
 		.init();
 
 	let database_url = get_secret(&secrets, "DATABASE_URL");
-	let config = aws_config::load_from_env().await;
+	let s3_config = aws_config::load_from_env().await;
 
 	let pool = PgPoolOptions::new()
 		.max_connections(50)
@@ -43,7 +43,7 @@ async fn main(#[shuttle_secrets::Secrets] secrets: SecretStore) -> shuttle_axum:
 		.expect("Failed to connect to database");
 
 	let app_state = AppState {
-		s3: aws_sdk_s3::Client::new(&config),
+		s3: aws_sdk_s3::Client::new(&s3_config),
 		pool,
 	};
 
@@ -51,7 +51,16 @@ async fn main(#[shuttle_secrets::Secrets] secrets: SecretStore) -> shuttle_axum:
 		.nest("/api", routes::router())
 		.layer(
 			ServiceBuilder::new()
-				.layer(TraceLayer::new_for_http())
+				.layer(
+					TraceLayer::new_for_http().make_span_with(|request: &Request<_>| {
+						tracing::info_span!(
+							"http_request",
+							"{} {}",
+							request.method(),
+							request.uri()
+						)
+					}),
+				)
 				.layer(TimeoutLayer::new(Duration::from_secs(120))),
 		)
 		.with_state(app_state);
