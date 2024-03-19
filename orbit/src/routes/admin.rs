4b0fb1696fa8194400ddd29a14fe76851604bc5a
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-03-19 19:31:05 -0400

    push remaining changes for archive

--- orbit/src/routes/admin.rs
+++ orbit/src/routes/admin.rs
@@ -1,13 +1,6 @@
-use axum::extract::Json;
-use axum::routing::post;
 use axum::Router;
-use orbit_types::models::user::{Color, CreateColor, Role};
-use orbit_types::Snowflake;
 
-use crate::auth::AuthUser;
-use crate::db::Conn;
-use crate::error::{JsonError, ResultExt};
-use crate::{AppState, Result};
+use crate::AppState;
 
 pub fn router() -> Router<AppState> {
 	Router::new()
